27aebc9b97a879c746d310cc175e45df2da442a5
Le Tuan Anh <tuananh.ke@gmail.com>
2016-05-18 15:50:26 +0800

    Add C track with a hello world

---
+++ trackc/001_hello_c/build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Compiling 'hello.c'"
+gcc hello.c -o hello
+
+echo "Executing 'hello' program"
+./hello
---
+++ trackc/001_hello_c/hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * This file can be compiled by using gcc. Try this:
+ *   gcc hello.c -o hello
+ * An executable file named `hello` will be generated. To run it, use this:
+ *   ./hello
+ **/
+
+int main(void)
+{
+  printf("Aloha World\n");
+  return 0;
+}
